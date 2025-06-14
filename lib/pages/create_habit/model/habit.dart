0130dbd1a7677dd1fc51a361704bbe428f4630bc
Selin Bartocha <bartocha.selin03@gmail.com>
2025-06-14 14:45:05 +0200

    sonstiges

--- lib/pages/create_habit/model/habit.dart
+++ lib/pages/create_habit/model/habit.dart
@@ -10,4 +10,11 @@
    required this.title,
     required this.createdAt,
   });
+  Map<String, dynamic> toMap() {
+   return {
+    'id': id,
+    'title': title,
+    'createdAt': createdAt.toIso8601String(),
+   };
+  }
 }
