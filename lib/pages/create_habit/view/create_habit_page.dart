20bf79cce5a02b970747b60640da4df098ec5f38
Selin Bartocha <bartocha.selin03@gmail.com>
2025-06-16 21:02:05 +0200

    Choice Chips hinzugefügt für die Tageszeitauswahl

--- lib/pages/create_habit/view/create_habit_page.dart
+++ lib/pages/create_habit/view/create_habit_page.dart
@@ -14,6 +14,10 @@ class _CreateHabitPageState extends State<CreateHabitPage> {
   final TextEditingController _habitNew = TextEditingController();
   final CreateHabitController _habitController = CreateHabitController();
 
+  bool isMorning = false;
+  bool isNoon = false;
+  bool isEvening = false;
+
   @override
   void dispose() {
     _habitNew.dispose();
@@ -24,9 +28,19 @@ class _CreateHabitPageState extends State<CreateHabitPage> {
     final text = _habitNew.text.trim();
 
     if (text.isNotEmpty) {
-      final habit = Habit(id: null, title: text, createdAt: DateTime.now());
+      final habit = Habit(
+        id: null,
+        title: text,
+        createdAt: DateTime.now(),
+        morning: isMorning,
+        noon: isNoon,
+        evening: isEvening,
+      );
       await _habitController.addHabit(habit);
       _habitNew.clear();
+      isMorning = false;
+      isNoon = false;
+      isEvening = false;
     }
   }
 
@@ -46,7 +60,24 @@ class _CreateHabitPageState extends State<CreateHabitPage> {
               ),
             ),
             const SizedBox(height: 20),
-
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ChoiceChip(label: const Text("Morgens"),
+                  selected: isMorning,
+                  onSelected: (value) => setState(() => isMorning = value),
+                ),
+                ChoiceChip(label: const Text("Mittags"),
+                  selected: isNoon,
+                  onSelected: (value) => setState(() => isNoon = value),
+                ),
+                ChoiceChip(label: const Text("Abends"),
+                  selected: isEvening,
+                  onSelected: (value) => setState(() => isEvening = value),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 _handleSubmitHabit();
