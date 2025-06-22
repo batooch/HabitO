780a13034c008eab47c305d8f6c010dfb870504c
Selin Bartocha <bartocha.selin03@gmail.com>
2025-06-22 13:33:59 +0200

    NavigationBar hinzugefügt

---
+++ lib/router/navigation_shell.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class NavigationShell extends StatelessWidget {
+  final Widget child;
+
+  const NavigationShell({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: child,
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (index) {
+          switch (index) {
+            case 0:
+              context.goNamed('home');
+              break;
+            case 1:
+              context.goNamed('create');
+              break;
+
+            case 2:
+              context.goNamed('statistic');
+              break;
+          }
+        },
+        selectedIndex: _calculateSelectedIndex(context),
+
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.add), label: 'Erstellen'),
+          NavigationDestination(
+            icon: Icon(Icons.bar_chart),
+            label: 'Übersicht',
+          ),
+        ],
+      ),
+    );
+  }
+
+  int _calculateSelectedIndex(BuildContext context) {
+    final location = GoRouterState.of(context).uri.toString();
+    if (location.startsWith('/home')) return 0;
+    if (location.startsWith('/create')) return 1;
+    if (location.startsWith('/statistic')) return 2;
+    return 0;
+  }
+}
