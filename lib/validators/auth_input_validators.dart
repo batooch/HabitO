52401a7c83c989c8176e07f88a781789a0c1baed
Selin Bartocha <bartocha.selin03@gmail.com>
2025-07-25 13:51:42 +0200

    Input Validierung für die Registrierung festgelegt

---
+++ lib/validators/auth_input_validators.dart
@@ -0,0 +1,46 @@
+class AuthInputValidators {
+  static String? validateName(String? name) {
+    if (name == null || name.trim().isEmpty) {
+      return 'Dieses Feld darf nicht leer sein';
+    }
+    return null;
+  }
+
+  static String? validateEmail(String? email) {
+    if (email == null || email.trim().isEmpty) {
+      return 'E-Mail darf nicht leer sein';
+    }
+
+    final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    if (!emailRegex.hasMatch(email.trim())) {
+      return 'Ungültige E-Mail-Adresse';
+    }
+
+    return null;
+  }
+
+  static String? validatePassword(String? password) {
+    if (password == null || password.isEmpty) {
+      return 'Passwort darf nicht leer sein';
+    }
+
+    final regex = RegExp(r'^(?=.*[0-9]).{6,}$');
+    if (!regex.hasMatch(password)) {
+      return 'Mind. 6 Zeichen & mind. 1 Ziffer erforderlich';
+    }
+
+    return null;
+  }
+
+  static String? validatePasswordRepeat(String? password, String? repeatPassword) {
+    if (repeatPassword == null || repeatPassword.isEmpty) {
+      return 'Bitte wiederhole dein Passwort';
+    }
+
+    if (password != repeatPassword) {
+      return 'Passwörter stimmen nicht überein';
+    }
+
+    return null;
+  }
+}
