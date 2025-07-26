8323b5cb4339add56115ed8816392ac2e4369a8c
Selin Bartocha <bartocha.selin03@gmail.com>
2025-07-26 19:39:44 +0200

    Widget logout_button.dart zum ausloggen implementiert

---
+++ lib/widgets/logout_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+import '../bloc/auth/auth_bloc.dart';
+import '../bloc/auth/auth_event.dart';
+import '../bloc/auth/auth_state.dart';
+
+class LogoutButton extends StatelessWidget {
+  const LogoutButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthBloc, AuthState>(
+      listenWhen:
+          (previous, current) =>
+              current is Unauthenticated || current is AuthError,
+      listener: (context, state) {
+        if (state is Unauthenticated) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Du wurdest erfolgreich abgemeldet.')),
+          );
+          context.goNamed('login');
+        } else if (state is AuthError) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.message)));
+        }
+      },
+      child: TextButton.icon(
+        onPressed: () {
+          context.read<AuthBloc>().add(LogoutRequested());
+        },
+        icon: const Icon(Icons.logout, color: Colors.red),
+        label: const Text('Logout', style: TextStyle(color: Colors.red)),
+      ),
+    );
+  }
+}
