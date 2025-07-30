1d624c51ea18f0c8f20275cbfa5e951c811f2c66
Selin Bartocha <bartocha.selin03@gmail.com>
2025-07-30 10:23:26 +0200

    color und text_style Konstanten erweitert

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Hauptfarbe für Buttons, Navigation etc.
+  static const Color primaryGreen = Color(0xFF19392A);
+
+  // Standard-Hintergrundfarbe
+  static const Color background = Color(0xFFFCF7F1);
+
+  // Weicherer Hintergrund (z.B. für Habit-Karten)
+  static const Color backgroundSoft = Color(0xFFFBF5EB);
+
+  //Fehlermeldungen
+  static const Color error = Colors.red;
+}
--- lib/constants/app_text_styles.dart
+++ lib/constants/app_text_styles.dart
@@ -1,44 +1,35 @@
 import 'package:flutter/material.dart';
+import 'app_colors.dart';
 
 class AppTextStyles {
   static const TextStyle headlineLarge = TextStyle(
-    fontSize: 24,
+    fontSize: 32,
     fontWeight: FontWeight.bold,
+    color: AppColors.primaryGreen,
   );
 
   static const TextStyle headlineMedium = TextStyle(
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: FontWeight.bold,
-  );
-
-  static const TextStyle headlineSmall = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.w600,
-  );
-
-  static const TextStyle bodyLarge = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.normal,
+    color: AppColors.primaryGreen,
   );
 
   static const TextStyle bodyMedium = TextStyle(
     fontSize: 14,
-    fontWeight: FontWeight.normal,
+    fontWeight: FontWeight.w500,
+    color: AppColors.primaryGreen,
+    height: 1.5,
   );
 
   static const TextStyle bodySmall = TextStyle(
     fontSize: 12,
-    fontWeight: FontWeight.normal,
-  );
-
-  static const TextStyle button = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.w600,
-  );
-
-  static const TextStyle label = TextStyle(
-    fontSize: 14,
     fontWeight: FontWeight.w500,
-    color: Colors.grey,
+    color: AppColors.primaryGreen,
+  );
+
+  static const TextStyle labelSmall = TextStyle(
+    fontSize: 8,
+    fontWeight: FontWeight.w500,
+    color: AppColors.primaryGreen,
   );
 }
